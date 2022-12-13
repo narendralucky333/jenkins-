9aa63690779463a02f097f4486a61f364e3bc807
narendralucky333 <97109645+narendralucky333@users.noreply.github.com>
2022-12-13 13:05:09 +0530

    Update SimpleServlet.java

--- src/main/java/org/codelikethewind/servlet/SimpleServlet.java
+++ src/main/java/org/codelikethewind/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h1>" + "Hi this is Narendra Lucky" + "</h1>");
+		out.println("<h1>" + "Hi World" + "</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
